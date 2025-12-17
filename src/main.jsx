88918cdb1c2518eaea36306bb00dc9152d2c7e7f
Ruby <liyu5649@gmail.com>
2025-12-17 16:24:20 +0800

    main.jsx 檔案調整<HashRouter>

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,10 @@ import App from './App.jsx'
 import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
-);
+)
+
